0491185b09231aea5036f42c50d86987861f5e74
seka7 <skananykhin@gmail.com>
2019-02-13 11:40:37 +0300

    Hotfix parsing bytes

--- wrappa/client/client.py
+++ wrappa/client/client.py
@@ -67,7 +67,7 @@ class Client:
 
             if b'-' in field_name:
                 obj_type, ind = field_name.split(b'-')
-                ind = str.join('', filter(lambda x: x.isdigit(), ind))
+                ind = int(ind)
             else:
                 obj_type, ind = field_name, None
 
